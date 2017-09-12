f4ae913b7b26dd0653ed557ddb518312d17738bc
MarceloAbio <marceloabio@yahoo.com.br>
2017-09-12 00:15:07 -0300

    Add files via upload

--- src/main/Planner.java
+++ src/main/Planner.java
@@ -11,8 +11,8 @@ public class Planner {
 		ArrayList<Student> studentDB = new ArrayList<Student>();
 		ArrayList<Teacher> teacherDB = new ArrayList<Teacher>();
 		int choose;
-		
-		System.out.println("Olá, seja bem-vindx ao Ballet Class Planner!");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Olï¿½, seja bem-vindx ao Ballet Class Planner!");
 		System.out.println("O que deseja fazer?");
 		System.out.println("1 - Login");
 		System.out.println("2 - Cadastrar");
@@ -24,7 +24,37 @@ public class Planner {
 		 * 
 		 * E melhor perguntar antes se e professor ou aluno ja que a pergunta e a mesma para ambos os casos?
 		 */
+		choose = in.nextInt();
+		switch(choose)
+		{
+		case 1: //login();
+				break;
+		case 2: sign_in();
+				break;
+		}
 
+	}
+
+	private static void sign_in() {
+		// TODO Auto-generated method stub
+		int type = question();
+		switch(type)
+		{
+		case 1: //sign_in_Student();
+				break;
+		case 2: //sign_in_Teacher();
+		}
+	}
+
+	private static int question() {
+		// TODO Auto-generated method stub
+		int choose;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Aluno ou professor?");
+		System.out.println("1 - Aluno");
+		System.out.println("2 - Professor");
+		choose = in.nextInt();
+		return choose;
 	}
 
 }
