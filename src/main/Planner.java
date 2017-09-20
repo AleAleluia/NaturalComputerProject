081b0ae50004b2c4e91855b0a7b8f773b165150f
Alessandra Aleluia <alessandra.aleluia@gmail.com>
2017-09-19 23:37:39 -0300

    Steps added
    Beta steps added

--- src/main/Planner.java
+++ src/main/Planner.java
@@ -10,9 +10,12 @@ public class Planner {
 		ArrayList<Step> stepDB = new ArrayList<Step>();
 		ArrayList<Student> studentDB = new ArrayList<Student>();
 		ArrayList<Teacher> teacherDB = new ArrayList<Teacher>();
+		
+		stepDB = fillSteps(stepDB);
+		
 		int choose;
 		Scanner in = new Scanner(System.in);
-		System.out.println("Olá, seja bem-vindo ao Ballet Class Planner!");
+		System.out.println("Ola, seja bem-vindo ao Ballet Class Planner!");
 		System.out.println("O que deseja fazer?");
 		System.out.println("1 - Login");
 		System.out.println("2 - Cadastrar");
@@ -27,10 +30,11 @@ public class Planner {
 		choose = in.nextInt();
 		switch(choose)
 		{
-		case 1: //login();
-				break;
-		case 2: sign_in(teacherDB,studentDB,stepDB);
-				break;
+			case 1: //login();
+				printSteps(stepDB);
+					break;
+			case 2: sign_in(teacherDB,studentDB,stepDB);
+					break;
 		}
 
 	}
@@ -80,5 +84,128 @@ public class Planner {
 		choose = in.nextInt();
 		return choose;
 	}
+	
+	private static ArrayList<Step> fillSteps(ArrayList<Step> StepList){
+		
+		Step step1 = new Step("Grand plie", 5);
+		StepList.add(step1);
+		
+		Step step2 = new Step("Tendu", 5);
+		StepList.add(step2);
+		
+		Step step3 = new Step("Jette", 5);
+		StepList.add(step3);
+		
+		Step step4 = new Step("Rond de jambe", 5);
+		StepList.add(step4);
+		
+		Step step5 = new Step("Retire", 5);
+		StepList.add(step5);
+		
+		Step step6 = new Step("Develope", 5);
+		StepList.add(step6);
+		
+		Step step7 = new Step("Arabesque", 5);
+		StepList.add(step7);
+		
+		Step step8 = new Step("Assemble", 5);
+		StepList.add(step8);
+		
+		Step step9 = new Step("Attitude", 5);
+		StepList.add(step9);
+		
+		Step step10 = new Step("Balance", 5);
+		StepList.add(step10);
+		
+		Step step11 = new Step("Ballone", 5);
+		StepList.add(step11);
+		
+		Step step12 = new Step("Ballote", 5);
+		StepList.add(step12);
+		
+		Step step13 = new Step("Cabriolle", 5);
+		StepList.add(step13);
+		
+		Step step14 = new Step("Cambre", 5);
+		StepList.add(step14);
+		
+		Step step15 = new Step("Changement", 5);
+		StepList.add(step15);
+		
+		Step step16 = new Step("Chasse", 5);
+		StepList.add(step16);
+		
+		Step step17 = new Step("Cloche", 5);
+		StepList.add(step17);
+		
+		Step step18 = new Step("Coupe", 5);
+		StepList.add(step18);
+		
+		Step step19 = new Step("Deboule", 5);
+		StepList.add(step19);
+		
+		Step step20 = new Step("Detire", 5);
+		StepList.add(step20);
+		
+		Step step21 = new Step("Echappe", 5);
+		StepList.add(step21);
+		
+		Step step22 = new Step("Eleve", 5);
+		StepList.add(step22);
+		
+		Step step23 = new Step("Emboite", 5);
+		StepList.add(step23);
+		
+		Step step24 = new Step("Fondu", 5);
+		StepList.add(step24);
+		
+		Step step25 = new Step("Fouette", 5);
+		StepList.add(step25);
+		
+		Step step26 = new Step("Frappe", 5);
+		StepList.add(step26);
+		
+		Step step27 = new Step("Glissade", 5);
+		StepList.add(step27);
+		
+		Step step28 = new Step("Pas de basque", 5);
+		StepList.add(step28);
+		
+		Step step29 = new Step("Pas de bouree", 5);
+		StepList.add(step29);
+		
+		Step step30 = new Step("Penche", 5);
+		StepList.add(step30);
+		
+		Step step31 = new Step("Pique", 5);
+		StepList.add(step31);
+		
+		Step step32 = new Step("Pirouette", 5);
+		StepList.add(step32);
+		
+		Step step33 = new Step("Promenade", 5);
+		StepList.add(step33);
+		
+		Step step34 = new Step("Releve", 5);
+		StepList.add(step34);
+		
+		Step step35 = new Step("Sissone", 5);
+		StepList.add(step35);
+		
+		Step step36 = new Step("Sous sous", 5);
+		StepList.add(step36);
+		
+		Step step37 = new Step("Tombe", 5);
+		StepList.add(step37);
+		
+		return StepList;
+	}
 
+	public static void printSteps(ArrayList<Step> StepList){
+		for(int i=0; i<37; i++){
+			System.out.print(i );
+			System.out.print(" - ");
+			System.out.println(StepList.get(i).getName());
+		}
+	}
 }
--- src/main/Step.java
+++ src/main/Step.java
@@ -3,13 +3,32 @@ package main;
 public class Step {
 	
 	String name; //nome do passo
-	String category; //categoria do passo (grand-plie, tendu, jette)
+	//String category; //categoria do passo (grand-plie, tendu, jette)
 	int exLevel;/*execute level 1 - não executa o passo
 								2 - executa muito mal
 								3 - executa normal
 								4 - executa bem
 								5 - executa muito bem*/
+	
+	public Step(String name, int exLevel) {
+		this.name = name;
+		this.exLevel = exLevel;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getExLevel() {
+		return exLevel;
+	}
+	public void setExLevel(int exLevel) {
+		this.exLevel = exLevel;
+	}
 
+	
+	
 	/*Metodos:
 	 * set exLevel
 	 * get exLevel
