4ef755757df233a88b7e38cb109c84b5f09685f0
Alessandra Aleluia <alessandra.aleluia@gmail.com>
2017-09-27 08:33:57 -0300

    Simulator changes started

--- src/main/Simulator.java
+++ src/main/Simulator.java
@@ -1,6 +1,13 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Simulator {
 	
 	//Algoritmo Genético aqui
+	Step chromo[] = new Step[8];
+	
+	public Step[] run(ArrayList<Step> Population){
+		return this.chromo;
+	}
 }
