977fbe67c2822d8603def15cccc447c96079346f
Alessandra Aleluia <alessandra.aleluia@gmail.com>
2017-10-02 09:43:55 -0300

    Some little changes (Simulator)

--- src/main/Simulator.java
+++ src/main/Simulator.java
@@ -5,9 +5,37 @@ import java.util.ArrayList;
 public class Simulator {
 	
 	//Algoritmo Genético aqui
-	Step chromo[] = new Step[8];
+	Step result[] = new Step[8];
 	
-	public Step[] run(ArrayList<Step> Population){
-		return this.chromo;
+	public Step[] run(ArrayList<Step> population){
+		population = improvement(population);
+		Step chromo[] = new Step[8];
+		
+		//Setar os cromossomos
+		
+		//Fazer mutacoes (se necessario)
+		
+		//Executar cruzamento (vai ter selecao elitista?)
+		
+		return this.result;
+	}
+	
+	public int howMuchToImprove(Step step){
+		int fitness, perfectStep = 5;
+		fitness = perfectStep - step.getExLevel();
+		return fitness;
+	}
+	
+	public ArrayList<Step> improvement(ArrayList<Step> stepList){
+		ArrayList<Step> improvementArray = new ArrayList<Step>();
+		improvementArray = (ArrayList<Step>)stepList.clone();
+		int improvement;
+		
+		for(int i=0; i<stepList.size(); i++){
+			improvement = howMuchToImprove(stepList.get(i));
+			improvementArray.get(i).setExLevel(improvement);
+		}
+		
+		return improvementArray;
 	}
 }
